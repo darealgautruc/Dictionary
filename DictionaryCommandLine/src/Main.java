ee0291ab5ea9fe99c943de7cecf2f6b431ccb15e
Huy Hoang <iamvhhoang@gmail.com>
2021-10-17 15:46:25 +0700

    Update Main
    update main.java

--- DictionaryCommandLine/src/Main.java
+++ DictionaryCommandLine/src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		Dictionary dic = new Dictionary();
+		DictionaryCommandline dim = new DictionaryCommandline();
+		
+		while(dic.word_list.size() >=0 ) {
+			dim.dictionaryBasic(dic);
+		}
 	}
 
 }
