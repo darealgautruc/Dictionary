65cda169cc0532f05e979e7d709fbb05992ad1dc
Huy Hoang <iamvhhoang@gmail.com>
2021-10-17 15:18:50 +0700

    them ham main

--- DictionaryCommandLine/src/Main.java
+++ DictionaryCommandLine/src/Main.java
@@ -1,9 +1,16 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< Updated upstream
 
+=======
+		Dictionary dic = new Dictionary();
+		DictionaryCommandline dim = new DictionaryCommandline();
+		while(dic.word_list.size() >=0 ) {
+			dim.dictionaryBasic(dic);
+		}
+>>>>>>> Stashed changes
 	}
 
 }
