4f15289cfcea28d4ae7652f079b483bfbe0d109f
ngocnguyen080701 <19020386@vnu.edu.vn>
2021-10-17 21:49:56 +0700

    đọc từ file text

--- DictionaryCommandLine/src/DictionaryManagement.java
+++ DictionaryCommandLine/src/DictionaryManagement.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class DictionaryManagement {
     public void insertFromCommandline(Dictionary dic) {
@@ -10,6 +12,17 @@ public class DictionaryManagement {
         TV = sc.nextLine();
         Word tuMoi = new Word(TA, TV);
         dic.word_list.add(tuMoi);
+    }
+    public void insertFromFile(Dictionary dic) throws FileNotFoundException {
+        System.setIn(new FileInputStream("dictionary.txt"));
+                Scanner sc = new Scanner(System.in);
+                Word word = new Word();
+                while (sc.hasNext()) {
+                        String s1 = sc.next();
+                        String s2 = sc.next();
+                        word.setWord_target(s1);
+                        word.setWord_explain(s2);
+                        }
     }
 
 }
--- DictionaryCommandLine/src/Main.java
+++ DictionaryCommandLine/src/Main.java
@@ -1,14 +1,18 @@
+import java.io.FileNotFoundException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Dictionary dic = new Dictionary();
 		DictionaryCommandline dim = new DictionaryCommandline();
-		
+		DictionaryManagement dm = new DictionaryManagement();
 		while(dic.word_list.size() >=0 ) {
+			dm.insertFromFile(dic);
 			dim.dictionaryBasic(dic);
+
 		}
+
 	}
 
 }
